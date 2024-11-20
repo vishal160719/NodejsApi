7527a51d26d7ad6b8226d47b5e1fcfb4885d51f8
Vishal <84004536+vishal160719@users.noreply.github.com>
2024-11-20 21:08:36 +0530

    Delete Downloads directory

--- Downloads/google-clone-flutter-master/android/app/src/main/kotlin/com/example/google_clone/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.google_clone
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Downloads/google-clone-flutter-master/lib/config/api_keys.dart
+++
@@ -1,2 +0,0 @@
-const String apiKey = "AIzaSyAvbH_LSwNs10WnUmIstPjYkOIAEfayBng";
-const String contextKey = "837db4cd6423547fb";
